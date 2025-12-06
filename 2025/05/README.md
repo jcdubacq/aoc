c144bfa5adb2a0412789ba3ce9cd392e77d792d9
Jean-Christophe Dubacq <jean-christophe.dubacq@ens-lyon.org>
2025-12-06 21:34:56 +0100

    Day 5

---
+++ 2025/05/README.md
@@ -0,0 +1,16 @@
+# Day 1
+
+[The challenge description](https://adventofcode.com/2025/day/5)
+
+```sh
+python3 p1.py < input.txt # last line is the answer
+python3 p2.py < input.txt # last line is the answer
+```
+
+**Status:** Complete
+
+## Remarks
+
+I solved part2 before part1, so I had a long time preparing the necessary data structures for part1. Note that I solved this on day 6, because life happens to be busy at times.
+
+I am pretty sur that there is a ton of enhancements to be done for a language like C (I use linear search instead of binary search), but the overhead of building trees in Python is just not useful for such a short computation.
---
+++ 2025/05/p1.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+import sys
+from warnings import catch_warnings
+from PIL import Image, ImageDraw
+from numpy import number
+
+# Two part loading
+firstpart = True
+intervals = []
+numbers = []
+for l in sys.stdin.readlines():
+    if len(l) < 2:
+        firstpart = False
+        continue
+    if firstpart:
+        intervals.append(l)
+    else:
+        numbers.append(int(l))
+
+
+class Interval:
+    def __init__(self, range_as_string) -> None:
+        e = range_as_string.split("-")
+        if len(e) < 2:
+            self.start = None
+            return
+        self.start = int(e[0])
+        self.stop = int(e[1])
+        try:
+            assert self.start <= self.stop
+        except AssertionError:
+            print(f"{range_as_string} is i515273536092893-515273536092893ncorrect")
+            sys.exit(1)
+
+    def inside(self, x: int):
+        if x < self.start:
+            return -1
+        if x > self.stop:
+            return 1
+        return 0
+
+    def defined(self) -> bool:
+        if self.start is None:
+            return False
+        return True
+
+    def __str__(self) -> str:
+        if self.defined():
+            return f"{self.start}-{self.stop}"
+        return "Empty range"
+
+
+class IntervalSet:
+    def __init__(self) -> None:
+        self.intervals = []
+
+    def append(self, i: Interval):
+        newlist = []
+        highers = []
+        build = True
+        for x in self.intervals:
+            left = i.inside(x.start)
+            right = i.inside(x.stop)
+            if right == -1:
+                newlist.append(x)
+            elif left == 1:
+                highers.append(x)
+            elif left == -1 and right == 1:  # i is contained in x
+                lowers = self.intervals
+                highers = []
+                build = False
+                break
+            elif left == 1 and right == -1:  # x is contained in i:
+                pass
+            else:
+                i.start = min(x.start, i.start)
+                i.stop = max(x.stop, i.stop)
+        if build:
+            self.intervals = newlist + [i] + highers
+
+    def inside(self, x: int):
+        for i in self.intervals:
+            if x > i.stop:
+                continue
+            if x < i.start:
+                return False
+            return True
+
+    def __str__(self) -> str:
+        return ",".join([str(x) for x in self.intervals])
+
+
+iset = IntervalSet()
+for i in intervals:
+    ii = Interval(i)
+    if ii.defined():
+        iset.append(ii)
+
+sum = 0
+for n in numbers:
+    if iset.inside(int(n)):
+        sum += 1
+print(sum)
